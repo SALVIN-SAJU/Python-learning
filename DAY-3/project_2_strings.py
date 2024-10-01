3b0134e8f4df925e3be7b0853b2b0a0a68c905c1
SALVIN-SAJU <salvinsajuofficial@gmail.com>
2024-10-01 09:01:53 +0000

    project  on strings

--- DAY-3/project_2_strings.py
+++ DAY-3/project_2_strings.py
@@ -1,3 +1,4 @@
 name: str = "Coders are ready ready"
+lower_name: str= name.lower()
 user_input: str = input("Enter a String to count : ")
-print(f" user_input in the string is = {name.count(user_input.lower())}")
+print(f" user_input in the string is = {lower_name.count(user_input.lower())}")
